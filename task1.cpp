7c1094fbad29b64ff56ad4f2c94c207301d42830
Oksana <oxemelyanovamx5@gmail.com>
2024-03-09 00:11:39 +0100

    initial commit

---
+++ task1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class Some_form
+{
+  protected:
+    std::string name = "Form";
+    int sides = 0;
+
+  public:
+    Some_form() {}
+    void how_many_sides()
+    {
+      std::cout << name << ": " << sides << std::endl;
+    }
+};
+
+class ChildTriangle : public Some_form
+{
+  public:
+    ChildTriangle()
+    {
+      Some_form::name = "Triangle";
+      Some_form::sides = 3;
+    }
+};
+
+class ChildRectangular : public Some_form
+{
+  public:
+    ChildRectangular()
+    {
+      Some_form::name = "Rectangular";
+      Some_form::sides = 4;
+    }
+};
+
+int main() {
+  Some_form f1;
+  f1.how_many_sides();
+
+  ChildTriangle chtr1;
+  chtr1.how_many_sides();
+
+  ChildRectangular chrect1;
+  chrect1.how_many_sides();
+
+  return (0); 
+}
